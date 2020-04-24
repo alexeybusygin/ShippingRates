c3c1939fbbd0d96bd48ed70f240ac9a7ab816d9a
alexeybusygin <alexey@busygin.com>
2020-04-24 07:50:11 +0200

    UPS Tests Fixed

--- ShippingRates.Tests/TestsConfiguration.cs
+++ ShippingRates.Tests/TestsConfiguration.cs
@@ -7,5 +7,8 @@ namespace ShippingRates.Tests
     public class TestsConfiguration
     {
         public string USPSUserId { get; set; }
+        public string UPSLicenseNumber { get; set; }
+        public string UPSUserId { get; set; }
+        public string UPSPassword { get; set; }
     }
 }
