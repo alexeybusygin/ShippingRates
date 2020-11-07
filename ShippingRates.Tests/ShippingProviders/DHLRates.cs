583aec4effe209cb9fd85e30b63b2617866d779f
alexeybusygin <alexey@busygin.com>
2020-11-07 20:18:52 +0100

    #6 Currency tests

--- ShippingRates.Tests/ShippingProviders/DHLRates.cs
+++ ShippingRates.Tests/ShippingProviders/DHLRates.cs
@@ -34,12 +34,12 @@ namespace ShippingRates.Tests.ShippingProviders
             var package = new Package(7, 7, 7, 6, 0);
 
             var r = _rateManager.GetRates(from, to, package);
-            var fedExRates = r.Rates.ToList();
+            var rates = r.Rates.ToList();
 
             Assert.NotNull(r);
-            Assert.True(fedExRates.Any());
+            Assert.True(rates.Any());
 
-            foreach (var rate in fedExRates)
+            foreach (var rate in rates)
             {
                 Assert.True(rate.TotalCharges > 0);
             }
@@ -53,10 +53,10 @@ namespace ShippingRates.Tests.ShippingProviders
             var package = new Package(7, 7, 7, 6, 1);
 
             var r = _rateManager.GetRates(from, to, package);
-            var fedExRates = r.Rates.ToList();
+            var rates = r.Rates.ToList();
 
             Assert.NotNull(r);
-            Assert.False(fedExRates.Any());
+            Assert.False(rates.Any());
             Assert.AreEqual(r.Errors.Count(), 3);
 
             var error = r.Errors.FirstOrDefault(r => r.Number == "420505");
@@ -65,26 +65,44 @@ namespace ShippingRates.Tests.ShippingProviders
             Assert.AreEqual(error.Description.Substring(0, 35), "The destination location is invalid");
         }
 
+        /*
         [Test]
         public async Task DHLSaturdayDelivery()
         {
             var from = new Address("", "", "75003", "FR");  // Paris to Lyon
             var to = new Address("", "", "69009", "FR");
-            var package = new Package(7, 7, 7, 6, 0);
+            //var package = new Package(7, 7, 7, 6, 1);
+            var package = new DocumentsPackage(5, 1);
 
             var today = DateTime.Now;
-            var nextFriday = today.AddDays(12 - (int)today.DayOfWeek);
+            var nextFriday = today.AddDays(11 - (int)today.DayOfWeek);
 
             var r = await _rateManager.GetRatesAsync(from, to, package, new ShipmentOptions()
             {
                 ShippingDate = nextFriday,
                 SaturdayDelivery = true
             });
-            var fedExRates = r.Rates.ToList();
+            var rates = r.Rates.ToList();
 
             Assert.NotNull(r);
-            Assert.True(fedExRates.Any());
-            Assert.True(fedExRates.Any(r => r.Options.SaturdayDelivery));
+            Assert.True(rates.Any());
+            Assert.True(rates.Any(r => r.Options.SaturdayDelivery));
+        }
+        */
+
+        [Test]
+        public async Task DHLCurrency()
+        {
+            var from = new Address("Amsterdam", "", "1043 AG", "NL");
+            var to = new Address("London", "", "SW1A 2AA", "GB");
+            var package = new Package(1, 1, 1, 5, 1);
+
+            var r = await _rateManager.GetRatesAsync(from, to, package);
+            var dhlRates = r.Rates.ToList();
+
+            Assert.NotNull(r);
+            Assert.True(dhlRates.Any());
+            Assert.False(dhlRates.Any(r => r.CurrencyCode != "EUR"));
         }
 
         [Test]
