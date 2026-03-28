deb5b12e276cb349e30c57a24814fcf36ef53750
AliD2024 <kardanadam933@gmail.com>
2026-03-28 22:18:05 +0100

    Unit tests for NullableEnum and DateTime JSON converters (#105)

---
+++ ShippingRates.Tests/Helpers/Json/NullableDateTimeConvertorTests.cs
@@ -0,0 +1,72 @@
+﻿using ShippingRates.Helpers.Json;
+using System.Text.Json;
+
+namespace ShippingRates.Tests.Helpers.Json;
+
+[TestFixture]
+public class NullableDateTimeConvertorTests
+{
+    [TestFixture]
+    public class NullableDateTimeConverterTests
+    {
+        private JsonSerializerOptions _options;
+
+        [SetUp]
+        public void Setup()
+        {
+            _options = new JsonSerializerOptions();
+            _options.Converters.Add(new NullableDateTimeConverter());
+        }
+
+        [Test]
+        public void Deserialization_ShouldHandleValidAndSpecialCases()
+        {
+            using (Assert.EnterMultipleScope())
+            {
+                var resultNull = JsonSerializer.Deserialize<DateTime?>("null", _options);
+                Assert.That(resultNull, Is.Null, "JSON 'null' should be deserialized as null.");
+
+                var resultEmpty = JsonSerializer.Deserialize<DateTime?>("\"\"", _options);
+                Assert.That(resultEmpty, Is.Null, "An empty string should be deserialized as null.");
+
+                var jsonDate = "\"2026-03-26T15:00:00Z\"";
+                var resultDate = JsonSerializer.Deserialize<DateTime?>(jsonDate, _options);
+
+                Assert.That(resultDate, Is.Not.Null, "A valid date string should not return null.");
+                Assert.That(resultDate?.Year, Is.EqualTo(2026), "The year should be 2026.");
+                Assert.That(resultDate?.Month, Is.EqualTo(3), "The month should be March.");
+            }
+        }
+
+        [Test]
+        public void Deserialization_InvalidDateValue_ThrowsJsonException()
+        {
+            var invalidDateJson = "\"not-a-valid-date\"";
+
+            var ex = Assert.Throws<JsonException>(() =>
+                JsonSerializer.Deserialize<DateTime?>(invalidDateJson, _options));
+
+            Assert.That(ex.Message, Does.Contain("Invalid date value"), "The exception message should indicate an invalid date.");
+        }
+
+        [Test]
+        public void Deserialization_UnexpectedToken_ThrowsJsonException()
+        {
+            var jsonObject = "{}";
+
+            Assert.Throws<JsonException>(() =>
+                JsonSerializer.Deserialize<DateTime?>(jsonObject, _options),
+                "An unexpected token (Object) should throw a JsonException.");
+        }
+
+        [Test]
+        public void Write_Always_ThrowsNotImplementedException()
+        {
+            DateTime? testDate = DateTime.UtcNow;
+
+            Assert.Throws<NotImplementedException>(() =>
+                JsonSerializer.Serialize(testDate, _options),
+                "The Write method should throw NotImplementedException as defined in the class.");
+        }
+    }
+}
---
+++ ShippingRates.Tests/Helpers/Json/NullableEnumJsonConvertorTests.cs
@@ -0,0 +1,64 @@
+﻿using ShippingRates.Helpers.Json;
+using System.Text.Json;
+
+namespace ShippingRates.Tests.Helpers.Json;
+
+[TestFixture]
+public class NullableEnumJsonConvertorTests
+{
+    private enum TestStatus
+    {
+        Active,
+        Pending
+    }
+
+    private JsonSerializerOptions _options;
+
+    [SetUp]
+    public void Setup()
+    {
+        _options = new JsonSerializerOptions();
+        _options.Converters.Add(new NullableEnumJsonConverter<TestStatus>());
+    }
+    [Test]
+    public void Deserialization_ShouldHandleAllValidAndSpecialCases()
+    {
+        using (Assert.EnterMultipleScope())
+        {
+            var resultNull = JsonSerializer.Deserialize<TestStatus?>("null", _options);
+            Assert.That(resultNull, Is.Null, "JSON 'null' should be deserialized as null.");
+
+            var resultEmpty = JsonSerializer.Deserialize<TestStatus?>("\"  \"", _options);
+            Assert.That(resultEmpty, Is.Null, "An empty or whitespace string should be deserialized as null.");
+
+            var resultValid = JsonSerializer.Deserialize<TestStatus?>("\"Active\"", _options);
+            Assert.That(resultValid, Is.EqualTo(TestStatus.Active), "A valid enum string should be correctly deserialized.");
+
+            var resultUnknown = JsonSerializer.Deserialize<TestStatus?>("\"NonExistentValue\"", _options);
+            Assert.That(resultUnknown, Is.Null, "An unknown enum string should be deserialized as null.");
+        }
+    }
+
+    [Test]
+    public void Deserialization_InvalidTokenType_ThrowsJsonException()
+    {
+        var jsonNumber = "123";
+
+        Assert.Throws<JsonException>(() =>
+            JsonSerializer.Deserialize<TestStatus?>(jsonNumber, _options),
+            "A JsonException should be thrown when the JSON token is not a string.");
+    }
+
+    [Test]
+    public void Serialization_ShouldWriteCorrectValues()
+    {
+        using (Assert.EnterMultipleScope())
+        {
+            var jsonValue = JsonSerializer.Serialize((TestStatus?)TestStatus.Active, _options);
+            Assert.That(jsonValue, Is.EqualTo("\"Active\""), "The enum value should be serialized as its string name.");
+
+            var jsonNull = JsonSerializer.Serialize((TestStatus?)null, _options);
+            Assert.That(jsonNull, Is.EqualTo("null"), "A null enum value should be serialized as 'null'.");
+        }
+    }
+}
